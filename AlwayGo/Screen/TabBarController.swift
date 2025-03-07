c42193937a0a624e368ce5c767aff2d17f8c31f7
Murad Abbaszada <abbaszademurad3@gmail.com>
2025-03-07 21:52:39 +0400

    tab bar controller added

---
+++ AlwayGo/Screen/TabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarController.swift
+//  AlwayGo
+//
+//  Created by Mac on 07.03.25.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
