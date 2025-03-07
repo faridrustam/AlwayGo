b48152bf68f818880656b44759a26700e42609f4
faridrustam <faridrustamov7442@gmail.com>
2025-03-07 21:24:25 +0400

    Project created

---
+++ AlwayGoUITests/AlwayGoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  AlwayGoUITestsLaunchTests.swift
+//  AlwayGoUITests
+//
+//  Created by Farid Rustamov on 07.03.25.
+//
+
+import XCTest
+
+final class AlwayGoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
