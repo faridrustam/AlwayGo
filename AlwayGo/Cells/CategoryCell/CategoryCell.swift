3553c9d6fb5bf6390c52f9f589cbf8d485c77d18
faridrustam <faridrustamov7442@gmail.com>
2025-03-08 11:31:52 +0400

    CategoryCell created

---
+++ AlwayGo/Cells/CategoryCell/CategoryCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCell.swift
+//  AlwayGo
+//
+//  Created by Farid Rustamov on 08.03.25.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
