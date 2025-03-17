28ca29919317da6cdfd2bfd09f86adde58d912f4
Murad Abbaszada <abbaszademurad3@gmail.com>
2025-03-17 11:55:12 +0400

    favorites controller configured

---
+++ AlwayGo/CustomClasses/FavoritesHeaderView.swift
@@ -0,0 +1,95 @@
+//
+//  FavoritesHeaderView.swift
+//  AlwayGo
+//
+//  Created by Mac on 16.03.25.
+//
+
+import UIKit
+
+class FavoritesHeaderView: UIView {
+    var isButtonSelected = false
+    
+    private lazy var labelStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.distribution = .fillProportionally
+        stack.spacing = 28
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    private lazy var allItemsButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("All Items", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont(name: "SFProText-Semibold", size: 16)
+        button.addTarget(self, action: #selector(allItemsButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var boardsButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Boards", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont(name: "SFProText-Semibold", size: 16)
+        button.addTarget(self, action: #selector(boardsButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var selectedButtonView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .app
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+        configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func allItemsButtonTapped(_ sender: UIButton) {
+        NSLayoutConstraint.activate([
+            selectedButtonView.topAnchor.constraint(equalTo: allItemsButton.bottomAnchor),
+            selectedButtonView.widthAnchor.constraint(equalToConstant: 152),
+            selectedButtonView.heightAnchor.constraint(equalToConstant: 3),
+            selectedButtonView.leadingAnchor.constraint(equalTo: labelStack.leadingAnchor)
+        ])
+    }
+    
+    @objc func boardsButtonTapped(_ sender: UIButton) {
+        NSLayoutConstraint.activate([
+            selectedButtonView.topAnchor.constraint(equalTo: boardsButton.bottomAnchor),
+            selectedButtonView.widthAnchor.constraint(equalToConstant: 152),
+            selectedButtonView.heightAnchor.constraint(equalToConstant: 3),
+            selectedButtonView.trailingAnchor.constraint(equalTo: labelStack.trailingAnchor)
+        ])
+    }
+    
+    private func configureUI() {
+        [labelStack, selectedButtonView].forEach({ addSubview($0) })
+        [allItemsButton, boardsButton].forEach({ labelStack.addArrangedSubview($0) })
+    }
+    
+    private func configureConstraints() {
+        NSLayoutConstraint.activate([
+            labelStack.topAnchor.constraint(equalTo: topAnchor),
+            labelStack.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 48),
+            labelStack.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -48),
+            labelStack.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+}
+
+#Preview {
+    FavoritesHeaderView()
+}
---
+++ AlwayGo/CustomClasses/ItemsView.swift
@@ -0,0 +1,55 @@
+//
+//  ItemsView.swift
+//  AlwayGo
+//
+//  Created by Mac on 17.03.25.
+//
+
+import UIKit
+
+class ItemsView: UIView {
+    private lazy var itemsCountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "15 items"
+        label.textColor = .lightGray
+        label.font = UIFont(name: "SFProText-Regular", size: 12)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var recentlyAddedButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Recently added", for: .normal)
+        button.setTitleColor(.lightGray, for: .normal)
+        button.titleLabel?.font = UIFont(name: "SFProText-Regular", size: 14)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+        configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        backgroundColor = .systemGray6
+        [itemsCountLabel, recentlyAddedButton].forEach({ addSubview($0) })
+    }
+    
+    private func configureConstraints() {
+        NSLayoutConstraint.activate([
+            itemsCountLabel.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            itemsCountLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            itemsCountLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -12),
+            
+            recentlyAddedButton.centerYAnchor.constraint(equalTo: itemsCountLabel.centerYAnchor),
+            recentlyAddedButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16)
+        ])
+    }
+}
