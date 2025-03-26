a20f10efed658874d9f8abf144ffb7118b4a937f
Murad Abbaszada <abbaszademurad3@gmail.com>
2025-03-26 20:31:36 +0400

    expandable cell configured

--- AlwayGo/Screen/Home/HomeController.swift
+++ AlwayGo/Screen/Home/HomeController.swift
@@ -14,7 +14,7 @@ class HomeController: BaseController {
     private lazy var searchView: UIView = {
         let view = UIView()
         view.backgroundColor = .white
-        view.layer.borderWidth = 0.5
+        view.layer.borderWidth = 1
         view.layer.cornerRadius = 25
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
@@ -30,6 +30,13 @@ class HomeController: BaseController {
     private lazy var searchTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "Search"
+        let attributedString = NSAttributedString(string: "Search",
+                                                  attributes: [NSAttributedString.Key.font: UIFont(name: "SFProText-Semibold", size: 16) ?? ""])
+        let attributedString2 = NSAttributedString(string: "Search",
+                                                   attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray])
+        textField.attributedPlaceholder = attributedString
+        textField.attributedPlaceholder = attributedString2
+        textField.textColor = .darkGray
         textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
@@ -151,6 +158,7 @@ extension HomeController: UICollectionViewDelegate, UICollectionViewDataSource, 
         case .forYou:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(ClothesCollectionCell.self)",
                                                           for: indexPath) as! ClothesCollectionCell
+            cell.configureCell(with: "For you")
             cell.handleSeeMoreButton = { [weak self] in
                 guard let self else { return }
                 let coordinator = SeeMoreCoordinator(navigationController: navigationController ?? UINavigationController())
@@ -171,6 +179,7 @@ extension HomeController: UICollectionViewDelegate, UICollectionViewDataSource, 
         case .recentlyViewed:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(ClothesCollectionCell.self)",
                                                           for: indexPath) as! ClothesCollectionCell
+            cell.configureCell(with: "Recently viewed")
             cell.handleSeeMoreButton = { [weak self] in
                 guard let self else { return }
                 let coordinator = SeeMoreCoordinator(navigationController: navigationController ?? UINavigationController())
@@ -191,6 +200,7 @@ extension HomeController: UICollectionViewDelegate, UICollectionViewDataSource, 
         case .trendingNow:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(ClothesCollectionCell.self)",
                                                           for: indexPath) as! ClothesCollectionCell
+            cell.configureCell(with: "Trending now")
             cell.handleSeeMoreButton = { [weak self] in
                 guard let self else { return }
                 let coordinator = SeeMoreCoordinator(navigationController: navigationController ?? UINavigationController())
@@ -206,6 +216,7 @@ extension HomeController: UICollectionViewDelegate, UICollectionViewDataSource, 
         case .bestDealsDiscounts:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(ClothesCollectionCell.self)",
                                                           for: indexPath) as! ClothesCollectionCell
+            cell.configureCell(with: "Best deals & Discounts")
             cell.handleSeeMoreButton = { [weak self] in
                 guard let self else { return }
                 let coordinator = SeeMoreCoordinator(navigationController: navigationController ?? UINavigationController())
--- AlwayGo/Screen/Product/ProductViewModel.swift
+++ AlwayGo/Screen/Product/ProductViewModel.swift
@@ -11,6 +11,10 @@ enum ProductCells {
     case color
     case size
     case info
+    case expandable(type: ExpandableCell)
+}
+
+enum ExpandableCell {
     case features
     case reviews
     case overviewAndVideos
@@ -18,17 +22,34 @@ enum ProductCells {
 }
 
 class ProductViewModel {
-    var model: [ProductCellModel] = [.init(cellInfo: "Features"),
-                                     .init(cellInfo: "Reviews"),
-                                     .init(cellInfo: "Overview and Videos"),
-                                     .init(cellInfo: "Photo")]
+    var model: [ProductCellModel] = [.init(cellName: .color,
+                                           cellInfo: [""],
+                                           isOpened: false),
+                                     .init(cellName: .size,
+                                           cellInfo: [""],
+                                           isOpened: false),
+                                     .init(cellName: .info,
+                                           cellInfo: [""],
+                                           isOpened: false),
+                                     .init(cellName: .expandable(type: .features),
+                                           cellInfo: ["Welcome to features"],
+                                           isOpened: false),
+                                     .init(cellName: .expandable(type: .reviews),
+                                           cellInfo: ["Welcome to reviews"],
+                                           isOpened: false),
+                                     .init(cellName: .expandable(type: .overviewAndVideos),
+                                           cellInfo: ["Welcome to overview and videos"],
+                                           isOpened: false),
+                                     .init(cellName: .expandable(type: .photos),
+                                           cellInfo: ["Welcome to photos"],
+                                           isOpened: false)]
     let cells: [ProductCells] = [
         .color,
         .size,
         .info,
-        .features,
-        .reviews,
-        .overviewAndVideos,
-        .photos
+        .expandable(type: .features),
+        .expandable(type: .reviews),
+        .expandable(type: .overviewAndVideos),
+        .expandable(type: .photos)
     ]
 }
